09a99af926a785f07e507fae48a6fe9bec7551ad
Anatoly Yakovlev <ihonhola@yandex.ru>
2026-04-08 19:44:04 +0300

    zadanie2

--- src/main/java/ru/yandex/practicum/assignment/avro/AvroAssignment2.java
+++ src/main/java/ru/yandex/practicum/assignment/avro/AvroAssignment2.java
@@ -1,10 +1,15 @@
 package ru.yandex.practicum.assignment.avro;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
 import ru.yandex.practicum.avro.DeviceAddedEvent;
 import ru.yandex.practicum.avro.DeviceRemovedEvent;
 import ru.yandex.practicum.avro.HubEvent;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.HexFormat;
 
@@ -17,8 +22,11 @@ public class AvroAssignment2 {
 
     public static HubEvent deserialize(byte[] bytes) throws IOException {
         // Реализуйте метод в соответствии с заданием
-        //     ...
-        return null;
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+        BinaryDecoder decoder = DecoderFactory.get().binaryDecoder(inputStream, null);
+        DatumReader<HubEvent> reader = new SpecificDatumReader<>(HubEvent.class);
+        return reader.read(null, decoder);
+        //return null;
     }
 
     // Этот метод нельзя менять
